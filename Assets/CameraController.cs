b28f1c3603ec6b24c87f723eac2f239cdadc611c
unknown <robbie.meyer12@gmail.com>
2019-06-01 11:25:05 -0400

    Implement morphing with one stage

---
+++ Assets/CameraController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float transitionDuration;
+
+    private float velocity;
+    private float target;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        velocity = 0.0f;
+        target = transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y != target)
+        {
+            float newPosition = Mathf.SmoothDamp(transform.position.y, target, ref velocity, transitionDuration);
+            transform.position = new Vector3(transform.position.x, newPosition, transform.position.z);
+        }
+    }
+
+    public void MoveDown(float distance)
+    {
+        velocity = 0.0f;
+        target = transform.position.y - distance;
+    }
+}
---
+++ Assets/Scripts/Stages/MorphController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+
+public class MorphController : MonoBehaviour
+{
+
+    public MorphController stage;
+    public float morphDuration;
+    public float morphDistance;
+
+    private CameraController maincamera;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        maincamera = Camera.main.gameObject.GetComponent<CameraController>();
+        Invoke("Morph", morphDuration);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Morph()
+    {
+        CreateStage();
+        maincamera.MoveDown(morphDistance);
+        Destroy(gameObject);
+    }
+
+    void CreateStage()
+    {
+        Vector3 position = gameObject.transform.position -  new Vector3(0, morphDistance, 0);
+        MorphController newStage = Instantiate(stage, position, Quaternion.identity);
+        
+        newStage.stage = stage;
+        newStage.morphDistance = morphDistance;
+        newStage.morphDuration = morphDuration; 
+    }
+
+    void GetStage()
+    {
+
+    }
+}
