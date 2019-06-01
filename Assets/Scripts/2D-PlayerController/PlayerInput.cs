fa40a0f15482512181586f185c132b6a7563943c
Daniel-U12 <daniel.utvich12@gmail.com>
2019-06-01 12:43:53 -0400

    moving & projectiles

--- Assets/Scripts/2D-PlayerController/PlayerInput.cs
+++ Assets/Scripts/2D-PlayerController/PlayerInput.cs
@@ -6,51 +6,61 @@ public class PlayerInput : MonoBehaviour
 {
 
     private PlayerController controller;
+    private Vector2 left = new Vector2 (-1,0);
+    private Vector2 right = new Vector2(1,0);
+    private Vector2 up = new Vector2(0,1);
+    private Vector2 down = new Vector2(0,-1);
     private Rigidbody2D rb;
-    public float jetpack_charge = 1;
+    private int dir;
+    
+    public float maxspeed = 1f;
+    public float jetpack_charge = 1.5f;
+    public float full_charge = 1.5f;
+    private float shoot_timer = 1.5f;
+    private bool reloaded = true;
     // Use this for initialization
     void Start()
     {
+        dir = -1;
+        controller = GetComponent<PlayerController>();
         rb = GetComponent<Rigidbody2D>();
+        Vector2 up = new Vector2(0,1);
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (!Input.GetButton("up") && jetpack_charge < 1){
-            jetpack_charge += (Time.deltaTime/2);
+        if(rb.velocity.magnitude > maxspeed) {
+            Vector2 unitVector = rb.velocity / rb.velocity.magnitude;
+            rb.velocity = unitVector * maxspeed;
         }
-        if (Input.GetButton("up") && jetpack_charge > 0) {
-            Vector2 up = new Vector2(0,1);
+        if (shoot_timer < 1.5f){
+            shoot_timer += Time.deltaTime;
+        }
+        if (!Input.GetButton("up") && jetpack_charge < full_charge){
+            jetpack_charge += (Time.deltaTime/1.75f);
+        }
+        if (Input.GetButton("up") && jetpack_charge > 0f) {
+            
             rb.AddForce(up*15);
             jetpack_charge -= Time.deltaTime;
         }
-        
-        // else if (Input.GetButtonDown("down")) {
-            
-        // }
-        // else if (Input.GetButtonDown("left")) {
-            
-        // }
-        // else if (Input.GetButtonDown("right")) {
-            
-        // }
-
-        // if (Input.GetButtonDown("Vertical" + playerNum.ToString()) < 0) {
-        //     controller.FastFall();
-        // }
-
-        // if (Input.GetButtonDown("Jump" + playerNum.ToString())) {
-        //     controller.JumpPressed();
-        // }
-        // else if (Input.GetButton("Jump" + playerNum.ToString())) {
-        //     controller.ContinueJump();
-        // }
-
-        // if (Input.GetButtonDown("Fire" + playerNum.ToString())) {
-        //     controller.Shoot();
-        // }
-
+        if (Input.GetButton("down")) {
+            //attack
+        }
+        if (Input.GetButton("left")) {
+            dir = -1;
+            rb.AddForce(left*15);
+        }
+        else if (Input.GetButton("right")) {
+            dir = 1;
+            rb.AddForce(right*15);
+        }
+        if (Input.GetButtonDown("fire") && shoot_timer >= 1.5f){
+            Debug.Log("fired");
+            shoot_timer = 0;
+            controller.shoot(dir);
+        }
     }
 
 }
--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,99 +1,99 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-using UnityEngine.SceneManagement;
-using UnityEngine.UI;
+﻿// using System.Collections;
+// using System.Collections.Generic;
+// using UnityEngine;
+// using UnityEngine.SceneManagement;
+// using UnityEngine.UI;
 
-public class GameManager : MonoBehaviour {
+// public class GameManager : MonoBehaviour {
 
 
-    [SerializeField]
-    string MainMenuScene;
+//     [SerializeField]
+//     string MainMenuScene;
 
-    bool isPaused;
+//     bool isPaused;
 
-    public GameObject PauseUI;
-    public GameObject EndUI;
-    public GameObject WinnerText;
-    public WinCondition winCondition;
-    private bool gameOver = false;
+//     public GameObject PauseUI;
+//     public GameObject EndUI;
+//     public GameObject WinnerText;
+//     public WinCondition winCondition;
+//     private bool gameOver = false;
 
-    AudioSource audioSource;
-    public GameObject gameSongObject;
-    AudioSource gameMusic;
-    public AudioClip winSong;
-    public AudioClip pauseSong;
-    public AudioClip readyGoClip;
+//     AudioSource audioSource;
+//     public GameObject gameSongObject;
+//     AudioSource gameMusic;
+//     public AudioClip winSong;
+//     public AudioClip pauseSong;
+//     public AudioClip readyGoClip;
 
-    private bool playOnce = true;
+//     private bool playOnce = true;
 
-	// Use this for initialization
-	void Start () {
-        isPaused = false;
-        winCondition = GetComponent<WinCondition>();
-        winCondition.OnRoundComplete += OnRoundCompleteHandler;
-        audioSource = GetComponent<AudioSource>();
-        gameMusic = gameSongObject.GetComponent<AudioSource>();
-    }
+// 	// Use this for initialization
+// 	void Start () {
+//         isPaused = false;
+//         winCondition = GetComponent<WinCondition>();
+//         winCondition.OnRoundComplete += OnRoundCompleteHandler;
+//         audioSource = GetComponent<AudioSource>();
+//         gameMusic = gameSongObject.GetComponent<AudioSource>();
+//     }
 
-    void Update () {
+//     void Update () {
 
-        if (playOnce) {
-            playOnce = false;
-            audioSource.clip = readyGoClip;
-            audioSource.loop = false;
-            audioSource.Play();
-        }
+//         if (playOnce) {
+//             playOnce = false;
+//             audioSource.clip = readyGoClip;
+//             audioSource.loop = false;
+//             audioSource.Play();
+//         }
 
-		if (Input.GetKeyDown("p") || Input.GetKeyDown("escape")) {
-            if (!gameOver) {
-                TogglePause();
-            }
-        }
-        else if (Input.GetKeyDown("r")) {
-            Restart();
-        }
-	}
+// 		if (Input.GetKeyDown("p") || Input.GetKeyDown("escape")) {
+//             if (!gameOver) {
+//                 TogglePause();
+//             }
+//         }
+//         else if (Input.GetKeyDown("r")) {
+//             Restart();
+//         }
+// 	}
 
-    public void TogglePause() {
-        audioSource.loop = true;
-        audioSource.clip = pauseSong;
-        isPaused = !isPaused;
-        Time.timeScale = isPaused ? 0 : 1;
-        PauseUI.SetActive(isPaused);
-        if (isPaused) {
-            audioSource.Play();
-            gameMusic.volume = 0;
-        } else {
-            audioSource.Stop();
-            gameMusic.volume = 1;
-        }
-    }
+//     public void TogglePause() {
+//         audioSource.loop = true;
+//         audioSource.clip = pauseSong;
+//         isPaused = !isPaused;
+//         Time.timeScale = isPaused ? 0 : 1;
+//         PauseUI.SetActive(isPaused);
+//         if (isPaused) {
+//             audioSource.Play();
+//             gameMusic.volume = 0;
+//         } else {
+//             audioSource.Stop();
+//             gameMusic.volume = 1;
+//         }
+//     }
 
-    public void Restart() {
-        Time.timeScale = 1f;
-        gameOver = false;
-        if (isPaused) {
-            TogglePause();
-        }
-        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
-    }
+//     public void Restart() {
+//         Time.timeScale = 1f;
+//         gameOver = false;
+//         if (isPaused) {
+//             TogglePause();
+//         }
+//         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+//     }
 
-    public void MainMenu() {
-        Time.timeScale = 1f;
-        SceneManager.LoadScene(MainMenuScene);
-    }
+//     public void MainMenu() {
+//         Time.timeScale = 1f;
+//         SceneManager.LoadScene(MainMenuScene);
+//     }
 
-    public void OnRoundCompleteHandler(string msg)
-    {
-        audioSource.loop = true;
-        audioSource.clip = winSong;
-        audioSource.Play();
-        gameMusic.volume = 0;
-        EndUI.SetActive(true);
-        gameOver = true;
-        WinnerText.GetComponent<Text>().text = msg;
-        //Time.timeScale = 0f;
-        Debug.Log(msg);
-    }
-}
+//     public void OnRoundCompleteHandler(string msg)
+//     {
+//         audioSource.loop = true;
+//         audioSource.clip = winSong;
+//         audioSource.Play();
+//         gameMusic.volume = 0;
+//         EndUI.SetActive(true);
+//         gameOver = true;
+//         WinnerText.GetComponent<Text>().text = msg;
+//         //Time.timeScale = 0f;
+//         Debug.Log(msg);
+//     }
+// }
