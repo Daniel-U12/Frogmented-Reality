096f8b07ac99b3324eb93c28fd58680263bcd774
Daniel-U12 <daniel.utvich12@gmail.com>
2019-06-01 23:02:44 -0400

    wrecking ball

---
+++ Assets/Scripts/rotator.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class rotator : MonoBehaviour
+{
+    private float z = 0;
+    private float dir = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Debug.Log(dir);
+        Debug.Log(gameObject.transform.rotation.z);
+        if (gameObject.transform.rotation.z >= 0.7){
+            dir = -1f;
+        } else if (gameObject.transform.rotation.z <= -0.7){
+            dir = 1f;
+        }
+        z += Time.deltaTime * 15 * 3 * dir;
+        gameObject.transform.rotation = Quaternion.Euler(0, 0, z);   
+    }
+}
