67800e6bff526af28205d6fd437f7a1b699f1d10
Daniel-U12 <daniel.utvich12@gmail.com>
2019-06-02 16:23:51 -0400

    lol pw1 wins

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement; // include so we can manipulate SceneManager
 
 public class GameController : MonoBehaviour
 {
@@ -20,9 +21,20 @@ public class GameController : MonoBehaviour
         if( player1 == null ){
             //Time.timeScale = 0;
             Debug.Log("Player 2 wins");
+            StartCoroutine(p2wins());
         } else if (player2 == null){
             //Time.timeScale = 0;
             Debug.Log("Player 1 wins");
+            StartCoroutine(p1wins());
         }
+    }
+
+    IEnumerator p1wins() {
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene("p1wins");
+    }
+    IEnumerator p2wins() {
+        yield return new WaitForSeconds(1.5f);
+        SceneManager.LoadScene("p2wins");
     }
 }
